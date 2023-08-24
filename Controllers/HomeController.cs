417d6d34426ece3f75a414d7bd20ba4085a27fee
Juan Pirir <pirirjuan86@gmail.com>
2023-08-23 23:38:43 -0600

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Barberia.Models;
+
+namespace Barberia.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult Compras()
+    {
+        return View();
+    }
+    public IActionResult Inventarioprod()
+    {
+        return View();
+    }
+    public IActionResult Iniciarsesion()
+    {
+        return View();
+    }
+
+    [HttpPost] // Cambio: Usar POST para procesar el formulario
+    public IActionResult CalcularTotal(decimal precio, int cantidad)
+    {
+        decimal total = precio * cantidad;
+        return Json(new { Total = total }); // Devolver el resultado como JSON
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Controllers/SalonController.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Barberia.Models;
+namespace Barberia.Controllers
+{
+    public class SalonController : Controller
+    {
+        private readonly ILogger<SalonController> _logger;
+
+        public SalonController(ILogger<SalonController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Registrocitas()
+        {
+            return View();
+        }
+        public IActionResult Citasdiarias()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
